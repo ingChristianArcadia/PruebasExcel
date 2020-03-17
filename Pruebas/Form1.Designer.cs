ba4c329093e089a611321fde0b9b7f6895a68312
Christian Arcadia <ing.arcadiapps@gmail.com>
2020-03-16 21:30:41 -0600

    Primer commit

---
+++ Pruebas/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace Pruebas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbId = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbNombre = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbCantidad = new System.Windows.Forms.TextBox();
+            this.dgvProductos = new System.Windows.Forms.DataGridView();
+            this.btbAgregar = new System.Windows.Forms.Button();
+            this.btnLeer = new System.Windows.Forms.Button();
+            this.btnCrear = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProductos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbId
+            // 
+            this.tbId.Location = new System.Drawing.Point(118, 38);
+            this.tbId.Name = "tbId";
+            this.tbId.Size = new System.Drawing.Size(100, 20);
+            this.tbId.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ID PRODUCTO";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(246, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "NOMBRE";
+            // 
+            // tbNombre
+            // 
+            this.tbNombre.Location = new System.Drawing.Point(306, 41);
+            this.tbNombre.Name = "tbNombre";
+            this.tbNombre.Size = new System.Drawing.Size(100, 20);
+            this.tbNombre.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(439, 44);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "CANTIDAD";
+            // 
+            // tbCantidad
+            // 
+            this.tbCantidad.Location = new System.Drawing.Point(516, 41);
+            this.tbCantidad.Name = "tbCantidad";
+            this.tbCantidad.Size = new System.Drawing.Size(100, 20);
+            this.tbCantidad.TabIndex = 4;
+            // 
+            // dgvProductos
+            // 
+            this.dgvProductos.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvProductos.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dgvProductos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProductos.Location = new System.Drawing.Point(33, 67);
+            this.dgvProductos.Name = "dgvProductos";
+            this.dgvProductos.Size = new System.Drawing.Size(583, 99);
+            this.dgvProductos.TabIndex = 19;
+            // 
+            // btbAgregar
+            // 
+            this.btbAgregar.Location = new System.Drawing.Point(622, 67);
+            this.btbAgregar.Name = "btbAgregar";
+            this.btbAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btbAgregar.TabIndex = 20;
+            this.btbAgregar.Text = "Agregar";
+            this.btbAgregar.UseVisualStyleBackColor = true;
+            this.btbAgregar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnLeer
+            // 
+            this.btnLeer.Location = new System.Drawing.Point(622, 96);
+            this.btnLeer.Name = "btnLeer";
+            this.btnLeer.Size = new System.Drawing.Size(75, 23);
+            this.btnLeer.TabIndex = 21;
+            this.btnLeer.Text = "Leer";
+            this.btnLeer.UseVisualStyleBackColor = true;
+            // 
+            // btnCrear
+            // 
+            this.btnCrear.Location = new System.Drawing.Point(623, 142);
+            this.btnCrear.Name = "btnCrear";
+            this.btnCrear.Size = new System.Drawing.Size(75, 23);
+            this.btnCrear.TabIndex = 22;
+            this.btnCrear.Text = "Crear";
+            this.btnCrear.UseVisualStyleBackColor = true;
+            this.btnCrear.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 185);
+            this.Controls.Add(this.btnCrear);
+            this.Controls.Add(this.btnLeer);
+            this.Controls.Add(this.btbAgregar);
+            this.Controls.Add(this.dgvProductos);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbCantidad);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbNombre);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbId);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProductos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbId;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbNombre;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbCantidad;
+        private System.Windows.Forms.DataGridView dgvProductos;
+        private System.Windows.Forms.Button btbAgregar;
+        private System.Windows.Forms.Button btnLeer;
+        private System.Windows.Forms.Button btnCrear;
+    }
+}
+
---
+++ Pruebas/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using IronXL;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Pruebas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            WorkBook archivoExcel = WorkBook.Create(ExcelFileFormat.XLS);
+            archivoExcel.Metadata.Author = "Arcadia";
+
+            WorkSheet hojaExcel = archivoExcel.CreateWorkSheet("Productos");
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string idProducto = tbId.Text;
+            string nombreProducto = tbNombre.Text;
+            string cantidadProducto = tbCantidad.Text;
+            this.dgvProductos.Rows.Add(idProducto,nombreProducto,cantidadProducto);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dgvProductos.Columns.Add("idProducto","ID");
+            dgvProductos.Columns.Add("nombreProducto", "PRODUCTO");
+            dgvProductos.Columns.Add("cantidadProducto", "CANTIDAD");
+        }
+    }
+}
